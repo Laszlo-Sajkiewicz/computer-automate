da5e31d7babab21ae9e4f0ae5ac105be85949cde
SAJKIEWICZ Laszlo <sajkiewl@hostname.iut2.upmf-grenoble.fr>
2015-06-10 09:59:55 +0200

    add a test file and .tar.bz2 compressing

--- archiving.sh
+++ archiving.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 d_gzip=$(ls | grep *.gz | wc -l);
-d_bz2=$(ls | grep *.bz2 | wc -l);
+d_bz2=$(ls | grep *.tar.bz2 | wc -l);
 
 #use:
 # -d : decompress
@@ -9,16 +9,19 @@ d_bz2=$(ls | grep *.bz2 | wc -l);
 
 
 if [[ "$1" == "-d" ]]; then
-	echo "decompress"
+	
 	if [[ $d_gzip -ne 0 ]]; then
 		gunzip *.gz #decompress *.gz
-	elif [[ $d_bz2 -ne 0 ]]; then
-		bunzip2 *.bz2	#decompress *.bz2	
+	elif [[ $d_bz2 -ne 0 ]]; then	
+		tar jxvf *.tar.bz2 # decompress *.tar.bz2
 	fi
 elif [[ "$1" == "-c" ]]; then
-	echo "compress"
+	
 	if [[ "$2" == "gzip" ]]; then
 		gzip $3
+	elif [[ "$2" == "bz2" ]]; then
+		
+		 tar jcvf $3.tar.bz2 $4
 	fi
 fi
 #compress = c
