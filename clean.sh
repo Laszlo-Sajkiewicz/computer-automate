127488138d7eb059e906d2a2870cfa13078de627
SAJKIEWICZ Laszlo <sajkiewl@hostname.iut2.upmf-grenoble.fr>
2015-06-10 11:54:11 +0200

    Added aac and mov extension.

--- clean.sh
+++ clean.sh
@@ -8,44 +8,76 @@ pic_1=$(ls *.jpg | wc -l);
 pic_2=$(ls *.png | wc -l);
 #music
 music_1=$(ls  *.mp3 | wc -l);
+music_2=$(ls  *.aac | wc -l);
 #movie
 movie_1=$(ls  *.avi | wc -l);
 movie_2=$(ls  *.wmv | wc -l);
-
+movie_3=$(ls  *.mp4 | wc -l);
+movie_4=$(ls  *.mov | wc -l);
 
 #PICTURE
-if [ $pic_1 -ne 0 ] || [ $pic_2 -ne 0 ]; then
-	echo 'ok'
+if [ $pic_1 -ne 0 ]; then
+	#JPG
 	if [ ! -d /~/picture ]; then
-		mkdir ~/picture #create a folder called picture
+		mkdir ~/picture 
 		mv *.jpg ~/picture
-		mv *.png ~/picture
 	elif [ -d /~/picture ]; then
 		mv *.jpg ~/picture
-		mv *.png ~/picture
 	fi
-
+elif [ $pic_2 -ne 0  ]; then
+	#PNG
+		if [ ! -d /~/picture ]; then
+			mkdir ~/picture 
+			mv *.png ~/picture
+		elif [ -d /~/picture ]; then
+			mv *.png ~/picture
+		fi
 #MUSIC
 elif [ $music_1 -ne 0 ]; then
+	#MP3
 		if [ ! -d /~/music ]; then
-			mkdir ~/music # create a folder called music
+			mkdir ~/music 
 			mv *.mp3 ~/music
 		elif [ -d /~/music ]; then
 			mv *.mp3 ~/music
 		fi
+elif [ $music_2 -ne 0 ]; then
+	#AAC
+		if [ ! -d /~/music ]; then
+			mkdir ~/music 
+			mv *.aac ~/music
+		elif [ -d /~/music ]; then
+			mv *.aac ~/music
+		fi
 #MOVIE
 elif [ $movie_1 -ne 0 ]; then
+	#AVI
 		if [ ! -d /~/movie ]; then
-			mkdir ~/movie # create a folder called movie
+			mkdir ~/movie 
 			mv *.avi ~/movie
 		elif [ -d /~/movie ]; then
 			mv *.avi ~/movie
 		fi
 elif [ $movie_2 -ne 0 ]; then
+	#WMV
 		if [ ! -d /~/movie ]; then
 			mkdir ~/movie 
 			mv *.wmv ~/movie
 		elif [ -d /~/movie ]; then
 			mv *.wmv ~/movie
 		fi
+elif [ $movie_3 -ne 0 ]; then
+	#MP4
+		if [ ! -d /~/movie ]; then
+			mkdir ~/movie 
+			mv *.mp4 ~/movie
+		elif [ -d /~/movie ]; then
+			mv *.mp4 ~/movie
+elif [ $movie_4 -ne 0 ]; then
+	#MOV 
+		if [ ! -d /~/movie ]; then
+			mkdir ~/movie 
+			mv *.mov ~/movie
+		elif [ -d /~/movie ]; then
+			mv *.mov ~/movie
 fi
