446719e9944a7fd1806b4c984c51dcc71fa0ecb6
SAJKIEWICZ Laszlo <laszlo-94@hotmail.fr>
2015-06-11 13:41:16 +0200

    Add bash_aliases file

---
+++ bash_aliases.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+alias bashrc='$EDITOR ~/.bashrc'
+alias bashaliases='$EDITOR ~/.bash_aliases'
+alias bin='cd /usr/bin ; ls'
+alias cp ='cp -i'
+alias mv='mv -i'
+alias ip='ifconfig'
+alias rm='rm -i'
+alias name='uname -a'
+
