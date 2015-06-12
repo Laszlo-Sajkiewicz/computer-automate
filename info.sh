9ea0b2ea6988e786e752d003d0cd0ca6ce50d5cd
SAJKIEWICZ Laszlo <laszlo-94@hotmail.fr>
2015-06-12 18:09:10 +0200

    Add colors

--- info.sh
+++ info.sh
@@ -8,19 +8,22 @@ swapFree=$(cat  /proc/meminfo  | grep SwapTotal | cut -c 14-28);
 name=$(cat /proc/cpuinfo | grep "model name" | cut -c 14-57 | uniq);
 nb_cpu=$(cat /proc/cpuinfo | grep "cpu cores" | cut -c 13-17 | uniq);
 cpu_mode=$(lscpu | grep "CPU op-mode" | cut -c 16-40);
-archi=$(lscpu | grep Architecture | cut -c 20-30)
+archi=$(lscpu | grep Architecture | cut -c 20-30);
+swapCached=$(cat /proc/meminfo | grep SwapCached | cut -c 20-29);
+
 if [[ "$1" == "-mem" ]]; then
-		echo 'Memory total :' $MemTotal
-		echo 'Free memory :' $freemem
-		echo 'Memory available :' $mem_av
+		echo -e '\033[1;31;40mMemory total : \033[0m' $MemTotal
+		echo -e '\033[1;31;40mFree memory : \033[0m' $freemem
+		echo -e '\033[1;31;40mMemory available :\033[0m' $mem_av
 elif [[ "$1" == "-swap" ]]; then
-		echo 'Swap Total :' $swapTotal
-		echo 'Free swap :' $swapFree
+		echo -e '\033[1;31;40mSwap Total :\033[0m' $swapTotal
+		echo -e '\033[1;31;40mFree swap :\033[0m' $swapFree
+		echo -e '\033[1;31;40mSwap cached :\033[0m' $swapCached
 elif [[ "$1" == "-cpu" ]]; then
-			echo 'Name of cpu :' $name
-			echo 'Number of cpu cores :' $nb_cpu
-			echo 'CPU op-mode :' $cpu_mode
-			echo 'Architecture :' $archi
+		echo -e '\033[1;31;40mName of cpu :\033[0m' $name
+		echo -e '\033[1;31;40mNumber of cpu cores :\033[0m' $nb_cpu
+		echo -e '\033[1;31;40mCPU op-mode :\033[0m' $cpu_mode
+		echo -e '\033[1;31;40mArchitecture :\033[0m' $archi
 elif [ "$1" == "-h" ] || [ "$1" == "" ]; then
 		echo -e "\033[34m\033[45m USE :\033[0m "
 	 	echo '-mem : Give some informations about the memory'
