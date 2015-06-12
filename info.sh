38ac60279e64df8aace7e7f701ef1db7e72e3801
SAJKIEWICZ Laszlo <laszlo-94@hotmail.fr>
2015-06-12 14:25:29 +0200

    I deleted some useless informations and we can write ./info.sh or ./info.sh -h for help

--- info.sh
+++ info.sh
@@ -5,6 +5,7 @@
 nb_ps=$(ps |wc -l);
 MemTotal=$( cat  /proc/meminfo  | grep MemTotal | cut -c 14-28);
 freemem=$( cat  /proc/meminfo  | grep MemFree | cut -c 14-28);
+mem_av=$(cat  /proc/meminfo  | grep MemAvailable | cut -c 14-28);
 swapTotal=$(cat  /proc/meminfo  | grep SwapTotal | cut -c 14-28);
 swapFree=$(cat  /proc/meminfo  | grep SwapTotal | cut -c 14-28);
 
@@ -13,16 +14,12 @@ if [[ "$1" == "-n" ]]; then
 elif [[ "$1" == "-mem" ]]; then
 		echo 'Memory total :' $MemTotal
 		echo 'Free memory :' $freemem
+		echo 'Memory available :' $mem_av
 elif [[ "$1" == "-swap" ]]; then
 		echo 'Swap Total :' $swapTotal
 		echo 'Free swap :' $swapFree
-elif [[ "$1" == "-h" ]]; then
+elif [ "$1" == "-h" ] || [ "$1" == "" ]; then
 		echo -e "\033[34m\033[45m USE :\033[0m "
-	 	echo ' -n : show the number of process'
-	 	echo '-lsp : show all process running'
-	 	echo '-s : show the size , EX: ./info.sh -s FILE'
-	 	echo '-f : searches the named input files , EX : ./info.sh -f  TEXT FILE '
-	 	echo '-i :  print  all  information'
 	 	echo '-mem : Give somme informations about the memory'
 	 	echo '-swap : Give somme informations about the swap'
 fi
