747e2e3865811888ca3e6780d548349b0681bccd
Laszlo Sajkiewicz <laszlo-94@hotmail.fr>
2015-07-09 14:28:58 +0200

    add comments

--- clean.py
+++ clean.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 # -*-coding:encodage -*
 import os
-
+#functions
 def launch_clean(): 
   os.system("./clean.sh")
   
 def launch_arch():
   os.system("./archiving.sh")
+  
+#main 
