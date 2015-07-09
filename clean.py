5ff0a766e15c779acec7d56305937b29e43637d6
Laszlo Sajkiewicz <laszlo-94@hotmail.fr>
2015-07-09 14:36:51 +0200

    Update clean.py

--- clean.py
+++ clean.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python
 # -*-coding:encodage -*
 import os
-#functions
+#function
 def launch_clean(): 
   os.system("./clean.sh")
-  
-def launch_arch():
-  os.system("./archiving.sh")
-  
-#main 
