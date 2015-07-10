cfe2fd837946985f31f99ed0e5a4be5d433834c5
Laszlo Sajkiewicz <laszlo-94@hotmail.fr>
2015-07-10 09:32:36 +0200

    add comments and github aliases

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
+#edit
 alias bashrc='$EDITOR ~/.bashrc'
 alias bashaliases='$EDITOR ~/.bash_aliases'
+#protect
 alias cp ='cp -i'
 alias mv='mv -i'
-alias ip='ifconfig'
 alias rm='rm -i'
-alias name='uname -a'
-alias ll='ls -la'
 alias rmd='rm -ri'
-alias fin=' grep '
-alias psfu='ps -f -u'
+#information
+alias ip='ifconfig'
+alias name='uname -a'
 alias psk=' ps >> ps.txt ; cat ps.txt | cut -c 1-11 ; rm ps.txt'
 alias ps1='echo $PS1'
+#other
+alias ll='ls -la'
+alias psfu='ps -f -u'
 alias h='history'
+#github
+alias gs="git status" 
+alias gc="git commit"
