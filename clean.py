825da6e63fa25c7f980eba45d36dcad44b34dc77
Laszlo Sajkiewicz <laszlo-94@hotmail.fr>
2015-07-10 08:15:48 +0200

    Update clean.py

--- clean.py
+++ clean.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python
 # -*-coding:encodage -*
 import os
-#function
-def launch_clean(): 
-  os.system("./clean.sh")
+
+ # We count the number of images for each type of image
+pic_1=os.popen("ls *.jpg | wc -l", "r").read()
+pic_2=os.popen("ls *.png | wc -l","r").read()
+
+# We count the number of music for each type of music
+music_1=os.popen("ls *.mp3 | wc -l","r").read()
+music_2=os.popen("ls *.aac | wc -l","r").read()
+
+# We count the number of movies for each type of movie
+movie_1=os.popen("ls *.avi | wc -l","r").read()
+movie_2=os.popen("ls *.wmv | wc -l","r").read()
+movie_3=os.popen("ls *.mp4 | wc -l","r").read()
+movie_4=os.popen("ls *.mov | wc -l","r").read()
+movie_5=os.popen("ls *.mkv | wc -l","r").read()
