7ece117b91d9cfa899a842945d070ae582d39f07
Laszlo Sajkiewicz <laszlo-94@hotmail.fr>
2015-07-24 09:08:08 +0200

    Delete archiving.py

--- archiving.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-# -*-coding:encodage -*
-import os
-
-d_gzip=os.popen("ls | grep *.gz | wc -l","r").read()
-d_bz2=os.popen("ls | grep *.tar.bz2 | wc -l","r").read()
-d_zip=os.popen("ls | grep *.zip | wc -l","r").read()
